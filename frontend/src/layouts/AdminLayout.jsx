0da06694b7f5887e2ab699db8d785c863c97dbc1
MED SALEM GHALASS <msghalas@gmail.com>
2025-03-14 18:52:57 +0000

    sidebar

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import AdminSidebar from "../pages/admin/AdminSidebar";
 
 const AdminLayout = ({ children }) => {
   const navigate = useNavigate();
@@ -7,24 +8,9 @@ const AdminLayout = ({ children }) => {
   return (
     <div className="row m-1">
       <div className="col-md-4 col-lg-3 col-xl-2">
-        <div className="card">
+        <div className="card mb-1">
           <div className="card-body">
-            <div className="d-flex flex-column gap-1">
-              {LIST_SIDEBAR.map((item, index) => (
-                <button
-                  key={index}
-                  onClick={() => navigate(item.link)}
-                  className={`btn btn-sm ${
-                    location.pathname === item.link
-                      ? "btn-primary"
-                      : "btn-outline-primary"
-                  } w-100 text-start`}
-                >
-                  <i className={`bi ${item?.icon} me-2`}></i>
-                  {item.title}
-                </button>
-              ))}
-            </div>
+            <AdminSidebar />
           </div>
         </div>
       </div>
@@ -39,9 +25,3 @@ const AdminLayout = ({ children }) => {
 };
 
 export default AdminLayout;
-
-const LIST_SIDEBAR = [
-  { link: "/admin", title: "Dashboard", icon: "bi-speedometer" },
-  { link: "/admin/users", title: "Utilisateurs", icon: "bi-people" },
-  { link: "/admin/sites", title: "Sites", icon: "bi-geo-alt-fill" },
-];
---
+++ frontend/src/pages/admin/AdminSidebar.jsx
@@ -0,0 +1,79 @@
+const LIST_SIDEBAR = [
+  { link: "/admin", title: "Dashboard", icon: "bi-speedometer" },
+  { link: "/admin/users", title: "Utilisateurs", icon: "bi-people" },
+  { link: "/admin/sites", title: "Sites", icon: "bi-geo-alt-fill" },
+];
+
+import React, { useState } from "react";
+import { Button, Offcanvas, Nav } from "react-bootstrap";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+const Sidebar = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleNavLinkClick = () => {
+    // Close the sidebar when a link is clicked on smaller screens
+    if (window.innerWidth < 992) {
+      // 992px corresponds to the 'md' breakpoint in Bootstrap
+      handleClose();
+    }
+  };
+
+  return (
+    <>
+      {/* Button to toggle the sidebar (only visible on smaller screens) */}
+      <Button
+        variant="outline-primary"
+        onClick={handleShow}
+        className="d-md-none" // Only show the button on smaller screens
+      >
+        <i className="bi-ui-checks-grid"></i>
+      </Button>
+
+      {/* Offcanvas Sidebar, always visible on md+ screens and toggled on smaller screens */}
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        placement="start"
+        responsive="md" // Automatically hide on screens smaller than 'md'
+      >
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Sidebar</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav defaultActiveKey="/admin" className="flex-column">
+            {LIST_SIDEBAR.map((item, index) => (
+              <div className="d-flex align-items-center" key={index}>
+                {location.pathname === item.link && (
+                  <i className="bi bi-grip-vertical text-primary "></i>
+                )}
+                <Nav.Link
+                  as={Link}
+                  to={item.link}
+                  onClick={() => {
+                    navigate(item.link);
+                    handleNavLinkClick();
+                  }}
+                  className={`nav-link ${
+                    location.pathname === item.link && "ps-0"
+                  }`}
+                >
+                  <i className={`bi ${item?.icon} me-2`}></i>
+                  {item.title}
+                </Nav.Link>
+              </div>
+            ))}
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+};
+
+export default Sidebar;
